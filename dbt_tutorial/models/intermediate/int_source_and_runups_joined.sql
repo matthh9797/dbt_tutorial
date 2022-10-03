c24d8eb5875f435181dd7ba5721f96753745ca06
matthh9797 <matthewh@netextremity.com>
2022-10-03 22:32:01 +0100

    Removed extra folder

--- dbt_tutorial/models/intermediate/int_source_and_runups_joined.sql
+++ dbt_tutorial/models/intermediate/int_source_and_runups_joined.sql
@@ -1,13 +1,13 @@
 with histrical_source_events as (
-    select * from {{ ref('stg_noaa_tsunami__historical_source_event') }}
+    select * from {{ ref('int_historical_source_events_filtered') }}
 )
 , historical_runups as (
-    select * from {{ ref('stg_noaa_tsunami__historical_runups') }}
+    select * from {{ ref('int_historical_runups_aggregated') }}
 )
-
 , join_runups_and_source_events as (
     select      s.*, r.* except(tsevent_id)
     from        histrical_source_events s   
     left join   historical_runups r  
     on          s.id = r.tsevent_id
 )
+select * from join_runups_and_source_events
---
+++ dbt_tutorial/models/marts/tsunamis.sql
@@ -0,0 +1,1 @@
+select * from {{ ref('int_source_and_runups_joined') }}
